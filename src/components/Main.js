31c39c5f6503ec48082ecaf62d1cc15eaa0b2bf7
SalomaoTech <salomao@taimber.com>
2024-08-03 12:03:37 -0300

    Primeiro commit

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+export default function Main() {
+
+    return (
+        <main>
+            <section>1</section>
+            <section>2</section>
+            <section>3</section>
+        </main>
+    );
+
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,7 @@
+export default function Nav(){
+
+    return (
+        <nav>Isto é uma navegação</nav>
+    );
+
+}
