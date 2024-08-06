7e3686f3c8635bbbfdd2185b1db85fda0f8eee7f
SalomaoTech <salomao@taimber.com>
2024-08-06 17:49:59 -0300

    Consome API via CEP

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,35 @@
+import React, { useState, useEffect } from "react";
+
 export default function Main() {
+
+    const [dados, setDados] = useState(null);
+
+    useEffect(() => {
+
+        fetch("https://viacep.com.br/ws/74683-465/json/")
+            .then((response) => response.json())
+            .then((data) => {
+                setDados(data);
+            });
+
+
+    }, []);
 
     return (
         <main>
             <section className={"menu-lateral"}>1</section>
-            <section className={"conteudo-centro"}>2</section>
+
+            <section className={"conteudo-centro"}>
+
+                CEP: {dados.cep}
+                <br />
+                RUA: {dados.logradouro}
+                <br />
+                COMPLEMENTO: {dados.complemento}
+                <br />
+                LOCALIDADE: {dados.localidade}
+
+            </section>
         </main>
     );
 
