61850ebd4586d917be9f658e732af75d57de7709
SalomaoTech <salomao@taimber.com>
2024-08-06 14:07:51 -0300

    Criando menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,31 @@
 export default function Nav() {
 
+    const frutas = [
+
+        { titulo: "Maçã", preco: 9.9 },
+        { titulo: "Uva", preco: 12.9 },
+
+    ]
+
     return (
         <nav>
             <ul>
-                <li>Home</li>
-                <li>Sobre nós</li>
-                <li>Contato</li>
+                <li>
+                    <a href="#">Produtos</a>
+                    <ul className="submenu">
+                        {
+                            frutas.map((fruta, index) => (
+                                <a href="#">{fruta.titulo + " R$: " + fruta.preco}</a>
+                            ))
+                        }
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Contato</a>
+                </li>
             </ul>
-        </nav>
+
+        </nav >
     );
 
 }
